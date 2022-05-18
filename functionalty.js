2a675dc39a305354b2d9e0a06cebdfdaa870d6a4
Ali-Eldeba <alieldeba375@gmail.com>
2022-05-18 14:07:32 +0200

    adding capitalize function

--- functionalty.js
+++ functionalty.js
@@ -116,7 +116,16 @@ function removeDuplicate(value) {
  */
 function reverseString(word) {
   return word.split("").reverse().join("");
+}
 
+  /*
+  * Making first letter capital in the word
+  *
+  * @param {string} word
+  * @returns {string}
+  */
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 // Numbers
