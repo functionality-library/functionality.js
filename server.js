ebf8e5d422d4aae7381d7c103cd9df713d3ecd39
Ali Eldeba <alieldeba375@gmail.com>
2022-05-20 20:14:14 +0200

    adding default file

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var StaticServer = require("static-server");
 var server = new StaticServer({
   rootPath: "./website/", // required, the root of the server file tree
   port: 8000, // required, the port to listen
-  // templates: {
-  //   notFound: "404.html", // optional, defaults to undefined
-  // },
+  templates: {
+    index: "./website/index.html", // optional, defaults to 'index.html'
+    // notFound: "404.html", // optional, defaults to undefined
+  },
 });
 
 server.start(function () {
-  console.log("Server Started At Port", server.port);
+  console.log("Server Started Please Go To: loacalehost:" + server.port);
 });
