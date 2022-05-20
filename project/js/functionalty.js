6bf182986fd8c3ee57031044e132bc9e2dd2717b
Ali-Eldeba <alieldeba375@gmail.com>
2022-05-20 11:08:26 +0200

    adding randomHex function

--- project/js/functionalty.js
+++ project/js/functionalty.js
@@ -37,6 +37,10 @@ function randomElementFrom(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+function randomHex() {
+  return `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")}`;
+}
+
 // Arrays
 
 /**
