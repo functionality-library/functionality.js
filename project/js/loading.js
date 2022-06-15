8a6e6bccaa98d89a6c2be57a59f35c250f042eae
Ali-Eldeba <alieldeba375@gmail.com>
2022-06-15 02:38:14 +0200

    adding new branch + updating directories

--- project/js/loading.js
+++
@@ -1,9 +0,0 @@
-let loading = document.querySelector(".loading-wrapper");
-let body = document.querySelector("body");
-window.onload = function () {
-  setTimeout(function () {
-    loading.style.display = "none";
-    body.style.height = "fit-content";
-    body.style.overflow = "auto";
-  }, 2000);
-};
--- website/js/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";var bar=document.getElementById("bar");
-"use strict";var loading=document.querySelector(".loading-wrapper"),body=document.querySelector("body");window.onload=function(){setTimeout(function(){loading.style.display="none",body.style.height="fit-content",body.style.overflow="auto"},2e3)};
-//# sourceMappingURL=main.js.map
