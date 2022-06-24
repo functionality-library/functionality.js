bcb27ebe0575c05314d595535da69a3932286545
Ali-Eldeba <alieldeba375@gmail.com>
2022-06-24 12:45:47 +0200

    updating buils

---
+++ lib/numbers/addCommas.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Returns Number With Commas To Facilitate Reading | e.g 1000000 => 1,000,000
+ *
+ * @param {number} number
+ * @returns {string}
+ */
+export default function addCommas(number: number): string;
---
+++ lib/user/getBirthDay.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Returns the Age From a Given Year | e.g 2007 -> 15
+ *
+ * @param {number} year
+ * @returns {number}
+ */
+export default function getBirthDay(year: number): number;
