7ce52ed3d98a9151b77e7f7af6ec74a4682a061e
Ali-Eldeba <alieldeba375@gmail.com>
2022-07-14 21:52:26 +0200

    adding npm keywords and copy (beta) function

--- project/user/copy.ts
+++ project/user/copy.ts
@@ -1,10 +1,8 @@
+/**
+ * It takes text to copy it to the user
+ *
+ * @param {string} text - The text to copy to the clipboard.
+ */
 export default function copy(text: string): void {
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log("Async: Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Async: Could not copy text: ", err);
-    }
-  );
+  navigator.clipboard.writeText(text);
 }
